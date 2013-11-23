7240182f1cfc4624eadf1aeffa213a386e3e5ede
Ox To A Cart <ox.to.a.cart@gmail.com>
2013-11-22 18:01:45 -0600

    Added npm dedupe to package.bash

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ To run:
 `./compile.bash ; node lantern.js`
 
 This starts a local proxy on port 8080 at which you can point your browser.
+
+To build a tarball (just to see how big it is) you can run:
+
+`./package.bash`
--- package.bash
+++ package.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# First dedupe npm repos
+npm dedupe
+
 # Packages up this app in a tar.gz, respecting the settings in .gitignore and .npmignore.
 tar --exclude-from=.gitignore --exclude-from=.npmignore -czf lantern.tar.gz .
